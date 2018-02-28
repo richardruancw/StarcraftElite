c68a1ee68da5408bd5a50faa8b57247c07956aaa
Mingxiang Chen <mingxiangchen@DN525f0p.SUNet>
2018-02-27 16:21:30 -0800

    adding some bugs :-)

--- main/main.py
+++ main/main.py
@@ -5,6 +5,7 @@ _path = os.path.dirname(os.path.abspath(__file__))
 _path_utils = "/".join(_path.split('/')[:-1])+"/utils/"
 _path_models = "/".join(_path.split('/')[:-1])+"/models/"
 _path_net = "/".join(_path.split('/')[:-1])+"/my_net/save_net.ckpt"
+_path_log = "/".join(_path.split('/')[:-1])+"log.txt"
 sys.path.insert(0, _path_utils)
 sys.path.insert(0, _path_models)
 from DDQN_model import DDQN
@@ -27,22 +28,26 @@ def run_loop(env, agent, max_episodes = 300, max_steps = 20000):
     start_time = time.time()
     step = 0
     saver = tf.train.Saver()
+    with open(_path_log, 'w+') as f:
+        f.write("Rewards\n")
     try:
         with tf.Session() as sess:
             # saver.restore(sess, _path_net)
             sess.run(tf.global_variables_initializer())
 
             for episode in xrange(max_episodes):
-                save_path = saver.save(sess, "my_net/save_net.ckpt")
+                save_path = saver.save(sess, _path_net)
                 print("Save to path: ", save_path)
 
                 observation = env.reset().reshape([-1])
+                total_reward = 0.0
 
                 while True:
                     if step>max_steps:
                         break
                     action = agent.choose_action(sess, observation)
                     observation_, reward = env.step(action)
+                    total_reward+=reward
                     observation_ = observation_.reshape([-1])
                     agent.store_transition(observation, action, reward, observation_)
                     if (step > 200) and (step % 20 == 0):
@@ -51,9 +56,12 @@ def run_loop(env, agent, max_episodes = 300, max_steps = 20000):
                     if env.last:
                         break
                     step += 1
-                    print "Episode: "+str(episode)+"  Step: "+str(step)
+                    # print "Episode: "+str(episode)+"  Step: "+str(step)
+                print "Episode: "+str(episode)+"  Reward: "+str(total_reward)
+                with open(_path_log, 'a+') as f:
+                    f.write("Episode: "+str(episode)+"  Reward: "+str(total_reward)+"\n")
             print('game over')
-            save_path = saver.save(sess, "my_net/save_net.ckpt")
+            save_path = saver.save(sess, _path_net)
             print("Save to path: ", save_path)
     except KeyboardInterrupt:
         pass
