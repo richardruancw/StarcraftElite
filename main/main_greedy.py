4a70a947106ea3acdc8d272987866360671b81a4
Mingxiang Chen <mingxiangchen@DN0a223962.SUNet>
2018-03-09 19:26:39 -0800

    fixed some parameters

--- main/main_greedy.py
+++ main/main_greedy.py
@@ -69,7 +69,8 @@ with sc2_env.SC2Env(map_name="MoveToBeacon",
                     step_mul=1,
                     visualize=True,
                     game_steps_per_episode=steps * step_mul) as env:
-    simpleSC = SimpleScEnvDiscrete(env)
+    simpleSC = SimpleScEnvDiscrete(env, split_base = 1)
+
     DDQN_agent = DDQN(simpleSC.num_actions, 17*64*64,
                       learning_rate=0.01,
                       reward_decay=0.9,
